2e4f0da6abdda8176901085ea1ff403f1310dc0a
jreinehr <reinehr@tk-schulsoftware.de>
2020-03-26 10:06:41 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'nativescript-plugin-firebase';
+import {getCurrentPushToken} from "nativescript-plugin-firebase";
 
 @Component({
     selector: 'app-ns',
@@ -13,6 +15,23 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        firebase.init({
+            showNotifications: true,
+            showNotificationsWhenInForeground: true,
+
+            onPushTokenReceivedCallback: (token) => {
+                console.log('[Firebase] onPushTokenReceivedCallback:', {token});
+            },
+
+            onMessageReceivedCallback: (message: firebase.Message) => {
+                console.log('[Firebase] onMessageReceivedCallback:', {message});
+            }
+        })
+            .then(() => {
+                console.log('[Firebase] Initialized');
+            })
+            .catch(error => {
+                console.log('[Firebase] Initialize', {error});
+            });
     }
 }

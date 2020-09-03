b473a98600ad659f064b05a55ab827f8792a6aa7
Jakob Reinehr <kajob@gmx.net>
2020-09-03 11:17:50 +0200

    Registrierung eines neuen Geräts: Erklärungen hinzugefügt und Umwandlung der Seriennummer in Hex

--- src/app/settings/scan.component.ts
+++ src/app/settings/scan.component.ts
@@ -30,15 +30,26 @@ export class ScanComponent implements OnInit {
     //     new BarcodeScanner().scan({});
     // }
     saveForm() {
+        console.log('registrating Device ... ', this.serialNumber);
+        const serialNumberString = this.serialNumber.replace(/\D/g, "");
+        const year = parseInt(serialNumberString.substr(0,2));
+        const week = parseInt(serialNumberString.substr(2,2));
+        const producer = parseInt(serialNumberString.substr(4,2));
+        const productNumber = parseInt(serialNumberString.substr(6,4));
+        const serialNumberDec = ((year * Math.pow(2, 6) + week) * Math.pow(2, 6) + producer) * Math.pow(2, 13) + productNumber;
+        console.log('registrating Device ... ', serialNumberDec);
+        const serialNumberHex = '0x' + serialNumberDec.toString(16);
+        console.log('registrating Device ... ', serialNumberHex);
+
+
         this.isLoading = true;
-        this.apiService.registerDevice(this.serialNumber, this.registrationKey, this.deviceName).subscribe(response => {
-            console.log('registring Device ... ', response);
+        this.apiService.registerDevice(serialNumberHex, this.registrationKey, this.deviceName).subscribe(response => {
+            console.log('registrating Device ... ', response);
             if (response === 'SUCCESS') {
                 this.router.navigate(['']);
             } else {
                 const options = {
                     title: response,
-                    // message: 'Race chosen: Unicorn',
                     okButtonText: 'OK'
                 };
                 alert(options);

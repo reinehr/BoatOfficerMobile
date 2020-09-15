cb8dea568522d7dccbb797b7981d72d0c374fede
Jakob Reinehr <kajob@gmx.net>
2020-09-15 13:57:54 +0200

    Typos

--- src/app/settings/scan.component.ts
+++ src/app/settings/scan.component.ts
@@ -31,21 +31,21 @@ export class ScanComponent implements OnInit {
     //     new BarcodeScanner().scan({});
     // }
     saveForm() {
-        console.log('registrating Device ... ', this.serialNumber);
+        console.log('registering Device ... ', this.serialNumber);
         const serialNumberString = this.serialNumber.replace(/\D/g, "");
         const year = parseInt(serialNumberString.substr(0,2));
         const week = parseInt(serialNumberString.substr(2,2));
         const producer = parseInt(serialNumberString.substr(4,2));
         const productNumber = parseInt(serialNumberString.substr(6,4));
         const serialNumberDec = ((year * Math.pow(2, 6) + week) * Math.pow(2, 6) + producer) * Math.pow(2, 13) + productNumber;
-        console.log('registrating Device ... ', serialNumberDec);
+        console.log('registering Device ... ', serialNumberDec);
         const serialNumberHex = '0x' + serialNumberDec.toString(16);
-        console.log('registrating Device ... ', serialNumberHex);
+        console.log('registering Device ... ', serialNumberHex);
 
 
         this.isLoading = true;
         this.apiService.registerDevice(serialNumberHex, this.registrationKey, this.deviceName).subscribe(response => {
-            console.log('registrating Device ... ', response);
+            console.log('registering Device ... ', response);
             if (response === 'SUCCESS') {
                 this.router.navigate(['']);
             } else {

6a86cf8908cbb4e5dfe88ae288271f237d473a1a
webadminThingsOfficer <webadmin@thingsofficer.com>
2021-02-04 17:40:38 +0100

    Synchronizing the numbers in registration process with the boatofficerId copied by the captain.

--- src/app/settings/signonaboat.component.ts
+++ src/app/settings/signonaboat.component.ts
@@ -32,21 +32,21 @@ export class SignonaboatComponent implements OnInit {
         console.log('Hire on a Boat ... ', this.boatofficerId);
         const boatofficerIdString = this.boatofficerId.replace(/[^(0-9a-gA-G)]/g, "");
         console.log('Hire on a Boat ... ', boatofficerIdString);
-        const serialNumberString = boatofficerIdString.substr(0, 10).replace(/\D/g, "");
-        console.log('Hire on a Boat ... ', serialNumberString);
-        const year = parseInt(serialNumberString.substr(0,2));
-        const week = parseInt(serialNumberString.substr(2,2));
-        const producer = parseInt(serialNumberString.substr(4,2));
-        const productNumber = parseInt(serialNumberString.substr(6,4));
-        const apiKey = boatofficerIdString.substr(10,5);
-        const serialNumberDec = ((year * Math.pow(2, 6) + week) * Math.pow(2, 6) + producer) * Math.pow(2, 13) + productNumber;
-        console.log('registering Device ... ', serialNumberDec);
-        const serialNumberHex = '0x' + serialNumberDec.toString(16);
-        console.log('registering Device ... ', serialNumberHex);
+        const serialNumberHexString = "0x"+boatofficerIdString.substr(0, 8).toLowerCase();
+        // console.log('Hire on a Boat ... ', serialNumberString);
+        // const year = parseInt(serialNumberString.substr(0,2));
+        // const week = parseInt(serialNumberString.substr(2,2));
+        // const producer = parseInt(serialNumberString.substr(4,2));
+        // const productNumber = parseInt(serialNumberString.substr(6,4));
+        const apiKey = boatofficerIdString.substr(8,5);
+        //const serialNumberDec = ((year * Math.pow(2, 6) + week) * Math.pow(2, 6) + producer) * Math.pow(2, 13) + productNumber;
+        //console.log('registering Device ... ', serialNumberDec);
+        //const serialNumberHex = '0x' + serialNumberDec.toString(16);
+        console.log('registering Device ... ', serialNumberHexString);
 
 
         this.isLoading = true;
-        this.apiService.addDeviceCandidate(serialNumberHex, apiKey).subscribe(response => {
+        this.apiService.addDeviceCandidate(serialNumberHexString, apiKey).subscribe(response => {
             console.log('sign up as candidate... ', response);
             if (response === 'SUCCESS') {
                 const options = {

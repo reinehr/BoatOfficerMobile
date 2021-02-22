2f8e998cb6bbca578efc2bd45712f6812be6e687
Jakob Reinehr <kajob@gmx.net>
2021-02-22 18:10:59 +0100

    merged 4 modules into app.module as components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,37 +2,62 @@ import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 import { NSEmptyOutletComponent } from 'nativescript-angular';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import {AlarmComponent} from "~/app/alarm/alarm.component";
+import {AppComponent} from "~/app/app.component";
+import {HomeComponent} from "~/app/home/home.component";
+import {SettingsComponent} from "~/app/settings/settings.component";
+import {BoatComponent} from "~/app/boat/boat.component";
+import {AuthComponent} from "~/app/settings/auth.component";
+import {ScanComponent} from "~/app/settings/scan.component";
+import {SignonaboatComponent} from "~/app/settings/signonaboat.component";
+import {AlertsettingsComponent} from "~/app/settings/alertsettings.component";
+import {LogoutComponent} from "~/app/settings/logout.component";
+import {ImpressumComponent} from "~/app/settings/impressum.component";
+import {PurchaseComponent} from "~/app/settings/purchase.component";
+import {EditpersonaldataComponent} from "~/app/settings/editpersonaldata.component";
+import {TtnmapperviewComponent} from "~/app/settings/ttnmapperview.component";
+import {BoatsettingsComponent} from "~/app/boat/boatsettings.component";
+import {BoatsettingssingleComponent} from "~/app/boat/boatsettingssingle.component";
+import {UploadpictureComponent} from "~/app/boat/uploadpicture.component";
+import {SensordetailsComponent} from "~/app/boat/sensordetails.component";
+import {GpsdetailsComponent} from "~/app/boat/gpsdetails.component";
+import {MapdetailsComponent} from "~/app/boat/mapdetails.component";
+import {EditalarmsettingsComponent} from "~/app/boat/editalarmsettings.component";
+import {EditdevicesettingsComponent} from "~/app/boat/editdevicesettings.component";
+import {EditusersComponent} from "~/app/boat/editusers.component";
+import {WebcamComponent} from "~/app/home/webcam.component";
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: '/(homeTab:home/default//alarmTab:alarm/default//boatTab:boat/default//settingsTab:settings/default)',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        component: NSEmptyOutletComponent,
-        loadChildren: () => import('~/app/home/home.module').then((m) => m.HomeModule),
-        outlet: 'homeTab'
-    },
-    {
-        path: 'alarm',
-        component: NSEmptyOutletComponent,
-        loadChildren: () => import('~/app/alarm/alarm.module').then((m) => m.AlarmModule),
-        outlet: 'alarmTab'
-    },
-    {
-        path: 'boat',
-        component: NSEmptyOutletComponent,
-        loadChildren: () => import('~/app/boat/boat.module').then((m) => m.BoatModule),
-        outlet: 'boatTab'
-    },
-    {
-        path: 'settings',
-        component: NSEmptyOutletComponent,
-        loadChildren: () => import('~/app/settings/settings.module').then((m) => m.SettingsModule),
-        outlet: 'settingsTab'
-    },
+    {path: '', redirectTo: '/(home:home//alarm:alarm//boat:boat//settings:settings)', pathMatch: 'full'},
+
+    {path: 'home', component: HomeComponent, outlet: 'home'},
+    {path: 'alarm', component: AlarmComponent, outlet: 'home'},
+    { path: 'mapdetails/:idDevice', component: MapdetailsComponent, outlet: 'home'},
+    { path: 'webcam/:idDevice', component: WebcamComponent, outlet: 'home'},
+
+    {path: 'alarm', component: AlarmComponent, outlet: 'alarm'},
+
+    {path: 'boat', component: BoatComponent, outlet: 'boat'},
+    {path: 'settings', component: BoatsettingsComponent, outlet: 'boat'},
+    {path: 'settingssingle/:idDevice', component: BoatsettingssingleComponent, outlet: 'boat'},
+    {path: 'uploadpicture/:idDevice', component: UploadpictureComponent, outlet: 'boat'},
+    {path: 'sensordetails/:idDevice/:field', component: SensordetailsComponent, outlet: 'boat'},
+    {path: 'gpsdetails/:idDevice/:field', component: GpsdetailsComponent, outlet: 'boat'},
+    {path: 'mapdetails/:idDevice', component: MapdetailsComponent, outlet: 'boat'},
+    {path: 'editalarmsettings/:idDevice/:field/:idAlarm', component: EditalarmsettingsComponent, outlet: 'boat'},
+    {path: 'editdevicesettings/:idDevice/:field', component: EditdevicesettingsComponent, outlet: 'boat'},
+    {path: 'editusers/:idDevice/:roleOrRequests', component: EditusersComponent, outlet: 'boat'},
+
+    {path: 'settings', component: SettingsComponent, outlet: 'settings'},
+    { path: 'auth', component: AuthComponent, outlet: 'settings' },
+    { path: 'scan', component: ScanComponent, outlet: 'settings' },
+    { path: 'signonaboat', component: SignonaboatComponent, outlet: 'settings' },
+    { path: 'alertsettings', component: AlertsettingsComponent, outlet: 'settings' },
+    { path: 'logout', component: LogoutComponent, outlet: 'settings' },
+    { path: 'impressum', component: ImpressumComponent, outlet: 'settings' },
+    { path: 'purchase', component: PurchaseComponent, outlet: 'settings' },
+    { path: 'editpersonaldata', component: EditpersonaldataComponent, outlet: 'settings' },
+    { path: 'ttnmapperview', component: TtnmapperviewComponent, outlet: 'settings'},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,38 @@ import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-// import { BoatComponent } from "./boat/boat.component";
+import { AlarmComponent } from "./alarm/alarm.component";
 
 import {NativeScriptHttpClientModule} from 'nativescript-angular/http-client';
 import {TimeagoModule, TimeagoIntl, TimeagoFormatter, TimeagoCustomFormatter} from 'ngx-timeago';
 
 // GMSServices.MapView
 import * as platform from 'tns-core-modules/platform';
+import {BoatComponent} from "~/app/boat/boat.component";
+import {HomeComponent} from "~/app/home/home.component";
+import {SettingsComponent} from "~/app/settings/settings.component";
+import {AuthComponent} from "~/app/settings/auth.component";
+import {ScanComponent} from "~/app/settings/scan.component";
+import {SignonaboatComponent} from "~/app/settings/signonaboat.component";
+import {LogoutComponent} from "~/app/settings/logout.component";
+import {AlertsettingsComponent} from "~/app/settings/alertsettings.component";
+import {ImpressumComponent} from "~/app/settings/impressum.component";
+import {PurchaseComponent} from "~/app/settings/purchase.component";
+import {EditpersonaldataComponent} from "~/app/settings/editpersonaldata.component";
+import {TtnmapperviewComponent} from "~/app/settings/ttnmapperview.component";
+import {NativeScriptUIChartModule} from "nativescript-ui-chart/angular";
+import {CommonModule} from "@angular/common";
+import {SensordetailsComponent} from "~/app/boat/sensordetails.component";
+import {GpsdetailsComponent} from "~/app/boat/gpsdetails.component";
+import {MapdetailsComponent} from "~/app/boat/mapdetails.component";
+import {BoatsettingsComponent} from "~/app/boat/boatsettings.component";
+import {BoatsettingssingleComponent} from "~/app/boat/boatsettingssingle.component";
+import {UploadpictureComponent} from "~/app/boat/uploadpicture.component";
+import {EditalarmsettingsComponent} from "~/app/boat/editalarmsettings.component";
+import {EditdevicesettingsComponent} from "~/app/boat/editdevicesettings.component";
+import {EditusersComponent} from "~/app/boat/editusers.component";
+import {BoatHeaderComponent} from "~/app/shared/ui/boat-header/boat-header.component";
+import {WebcamComponent} from "~/app/home/webcam.component";
 // import * as GMSServices from 'nativescript-google-maps-sdk';
 if (platform.isIOS) {
 //     GMSServices.
@@ -44,6 +69,9 @@ export class MyIntl extends TimeagoIntl {
         ReactiveFormsModule,
         AppRoutingModule,
         NativeScriptHttpClientModule,
+        NativeScriptUIChartModule,
+        CommonModule,
+        NativeScriptFormsModule,
         TimeagoModule.forRoot({
             intl: { provide: TimeagoIntl, useClass: MyIntl },
             formatter: { provide: TimeagoFormatter, useClass: TimeagoCustomFormatter },
@@ -51,7 +79,10 @@ export class MyIntl extends TimeagoIntl {
     ],
     declarations: [
         AppComponent,
-        // BoatComponent,
+        AlarmComponent,
+        BoatComponent, SensordetailsComponent, GpsdetailsComponent, MapdetailsComponent, BoatsettingsComponent, BoatsettingssingleComponent, UploadpictureComponent, EditalarmsettingsComponent, EditdevicesettingsComponent, EditusersComponent, BoatHeaderComponent,
+        HomeComponent, WebcamComponent,
+        SettingsComponent, AuthComponent, ScanComponent, SignonaboatComponent, LogoutComponent, AlertsettingsComponent, ImpressumComponent, PurchaseComponent, EditpersonaldataComponent, TtnmapperviewComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
